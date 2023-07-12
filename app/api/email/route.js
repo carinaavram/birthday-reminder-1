e7d50c5a1513afb171ab47d3a55b8209b0f83778
carinaavram <carinaavram97@gmail.com>
2023-07-12 12:48:33 -0400

    email function

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -6,7 +6,7 @@ import sendgrid from '@sendgrid/mail';
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default async function handler(res, req) {
+export async function GET(res, req) {
   try {
     dbConnect();
     const users = await User.find();
