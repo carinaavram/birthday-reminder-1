eeb5067815274abf5be3aab55b78dfd1d46963ae
carinaavram <carinaavram97@gmail.com>
2023-07-12 12:34:34 -0400

    email function

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -113,7 +113,7 @@ export async function GET(req, res) {
       }
     }
     // Email sent successfully
-    return new NextResponse.json('Email sent succesfully!', { status: 200 });
+    return new NextResponse('Email sent succesfully!', { status: 200 });
   } catch (error) {
     console.error('Error sending emails:', error);
     // Handle errors
