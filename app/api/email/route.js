f13975b33688b3a31f8919e431a5c2829a637cc4
carinaavram <carinaavram97@gmail.com>
2023-07-12 15:38:59 -0400

    email function

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -36,38 +36,7 @@ export async function GET(req, res) {
           day: 'numeric',
         });
 
-        // const msg = {
-        //   to: user.email,
-        //   from: process.env.MY_EMAIL,
-        //   subject: `Birthday Reminder: ${birthday.name}'s birthday is coming up!`,
-        //   html: `
-        //   <html>
-        //     <head>
-        //       <style>
-        //         body {
-        //           font-family: Arial, sans-serif;
-        //           background-color: #whitesmoke;
-        //           color: #333;
-        //         }
-        //         h1 {
-        //           color: #ff5500;
-        //         }
-        //         p {
-        //           font-size: 16px;
-        //           line-height: 1.5;
-        //         }
-        //       </style>
-        //     </head>
-        //     <body>
-        //       <h1>Dear ${user.name},</h1>
-        //       <p>Just a reminder that ${birthday.name}'s birthday is coming up on ${formattedDate}.</p>
-        //       <p>Don't forget to wish ${birthday.name} a happy birthday!</p>
-        //       <p>Here are some gift ideas: ${birthday.gifts.join(',')}.</p>
-        //     </body>
-        //   </html>
-        // `,
-        // }
-        await sendgrid.send({
+        sendgrid.send({
           to: user.email,
           from: process.env.MY_EMAIL,
           subject: `Birthday Reminder: ${birthday.name}'s birthday is coming up!`,
