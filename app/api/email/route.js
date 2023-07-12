9ca128817fdad66f186b44d2bb7a13934ab5d1ef
carinaavram <carinaavram97@gmail.com>
2023-07-12 12:40:20 -0400

    email again

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -1,4 +1,3 @@
-import { NextResponse } from 'next/server';
 import { addDays } from 'date-fns';
 import dbConnect from '@/config/dbConnect';
 import User from '@/models/user';
@@ -36,37 +35,6 @@ export async function GET(req, res) {
           day: 'numeric',
         });
 
-        // const msg = {
-        //   to: user.email,
-        //   from: process.env.MY_EMAIL,
-        //   subject: `Birthday Reminder: ${birthday.name}'s birthday is coming up!`,
-        //   html: `
-        //   <html>
-        //     <head>
-        //       <style>
-        //         body {
-        //           font-family: Arial, sans-serif;
-        //           background-color: #whitesmoke;
-        //           color: #333;
-        //         }
-        //         h1 {
-        //           color: #ff5500;
-        //         }
-        //         p {
-        //           font-size: 16px;
-        //           line-height: 1.5;
-        //         }
-        //       </style>
-        //     </head>
-        //     <body>
-        //       <h1>Dear ${user.name},</h1>
-        //       <p>Just a reminder that ${birthday.name}'s birthday is coming up on ${formattedDate}.</p>
-        //       <p>Don't forget to wish ${birthday.name} a happy birthday!</p>
-        //       <p>Here are some gift ideas: ${birthday.gifts.join(',')}.</p>
-        //     </body>
-        //   </html>
-        // `,
-        // }
         await sendgrid.send(
           {
             to: user.email,
@@ -113,10 +81,10 @@ export async function GET(req, res) {
       }
     }
     // Email sent successfully
-    return new NextResponse('Email sent succesfully!', { status: 200 });
+    return res.status(200).json('Emails sent successfully!');
   } catch (error) {
     console.error('Error sending emails:', error);
     // Handle errors
-    return new NextResponse('Failed to send email', { status: 500 });
+    return res.status(500).json('Failed to send email');
   }
 }
