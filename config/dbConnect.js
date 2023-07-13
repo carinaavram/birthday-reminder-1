3bdd2ef5520f32a78f58bd2f51e643554023184a
carinaavram <carinaavram97@gmail.com>
2023-07-13 09:32:09 -0400

    updated connection

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const dbConnect = async () => {
-    try {
-      if (mongoose.connection.readyState >= 1) return;
-       await mongoose.connect(`${process.env.DB_URI}`);
-    } catch (error) {
-      console.error("Database connection error:", error);
-    }
-  };
+  try {
+    if (mongoose.connection.readyState >= 1) return;
+    await mongoose.connect(`${process.env.DB_URI}`);
+  } catch (error) {
+    throw new Error('Database connection error');
+  }
+};
 
 export default dbConnect;
