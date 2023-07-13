eb43d474a52580a75a616286788b6d70e9eefe15
carinaavram <carinaavram97@gmail.com>
2023-07-13 13:50:18 -0400

    changes in the email function

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -21,52 +21,53 @@ export async function GET(req, res) {
         const notificationDate = addDays(birthdayDate, -user.notificationDays);
         const today = new Date();
         if (
-          today.getDate() !== notificationDate.getDate() ||
-          (today.getMonth() + 1) !== (notificationDate.getMonth() + 1)
+          today.getDate() === notificationDate.getDate() &&
+          today.getMonth() + 1 === notificationDate.getMonth() + 1
         ) {
-          continue;
-        }
-        const formattedDate = birthdayDate.toLocaleDateString('en-US', {
-          weekday: 'long',
-          year: 'numeric',
-          month: 'short',
-          day: 'numeric',
-        });
+          const formattedDate = birthdayDate.toLocaleDateString('en-US', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+          });
 
-        
-        await sendgrid.send({
-          to: user.email,
-          from: process.env.MY_EMAIL,
-          subject: `Birthday Reminder: ${birthday.name}'s birthday is coming up!`,
-          html: `
-          <html>
-            <head>
-              <style>
-                body {
-                  font-family: Arial, sans-serif;
-                  background-color: #whitesmoke;
-                  color: #333;
-                }
-                h1 {
-                  color: #ff5500;
-                }
-                p {
-                  font-size: 16px;
-                  line-height: 1.5;
-                }
-              </style>
-            </head>
-            <body>
-              <h1>Dear ${user.name},</h1>
-              <p>Just a reminder that ${
-                birthday.name
-              }'s birthday is coming up on ${formattedDate}.</p>
-              <p>Don't forget to wish ${birthday.name} a happy birthday!</p>
-              <p>Here are some gift ideas: ${birthday.gifts.join(',')}.</p>
-            </body>
-          </html>
-        `,
-        });
+          // Uncomment the code block to send emails
+          await sendgrid.send({
+            to: user.email,
+            from: process.env.MY_EMAIL,
+            subject: `Birthday Reminder: ${birthday.name}'s birthday is coming up!`,
+            html: `
+            <html>
+              <head>
+                <style>
+                  body {
+                    font-family: Arial, sans-serif;
+                    background-color: #whitesmoke;
+                    color: #333;
+                  }
+                  h1 {
+                    color: #ff5500;
+                  }
+                  p {
+                    font-size: 16px;
+                    line-height: 1.5;
+                  }
+                </style>
+              </head>
+              <body>
+                <h1>Dear ${user.name},</h1>
+                <p>Just a reminder that ${
+                  birthday.name
+                }'s birthday is coming up on ${formattedDate}.</p>
+                <p>Don't forget to wish ${birthday.name} a happy birthday!</p>
+                <p>Here are some gift ideas: ${birthday.gifts.join(',')}.</p>
+              </body>
+            </html>
+            `,
+          });
+
+          console.log(`Birthday Reminder email sent to ${user.email}`);
+        }
       }
     }
     // Email sent successfully
