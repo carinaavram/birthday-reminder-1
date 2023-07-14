fe19bf2df74528a2c99f3574f52ce0a845afe5b6
carinaavram <carinaavram97@gmail.com>
2023-07-14 10:22:04 -0400

    email update

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -96,7 +96,7 @@ export async function GET(req, res) {
         subject: 'Test Email',
         html: '<h1>This is a test</h1>'
       });
-      return new NextResponse('Success', { status: 500 });
+      return new NextResponse('Success', { status: 200 });
     } catch(error){
       return new NextResponse('Failed to sent emails!', error);
     }
