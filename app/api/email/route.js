08bd13d18ad91a680be247875086644faae7f5e7
carinaavram <carinaavram97@gmail.com>
2023-07-14 11:18:40 -0400

    email function

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -74,22 +74,64 @@ async function sendEmails(emailData) {
   }
 }
 
-async function handler(req, res) {
-  const emailData = await getEmailData();
+export async function GET(req, res) {
+  await dbConnect();
+  const users = await User.find();
+  for (const user of users) {
+    if (!user.receiveEmailNotification) {
+      continue;
+    }
+    const birthdays = await Birthday.find({ email: user.email });
+    for (const birthday of birthdays) {
+      const birthdayDate = new Date(birthday.date);
+      const notificationDate = addDays(birthdayDate, -user.notificationDays);
+      const today = new Date();
+      if (
+        today.getDate() === notificationDate.getDate() &&
+        today.getMonth() + 1 === notificationDate.getMonth() + 1
+      ) {
+        const formattedDate = birthdayDate.toLocaleDateString('en-US', {
+          weekday: 'long',
+          year: 'numeric',
+          month: 'short',
+          day: 'numeric',
+        });
 
-  const response = await sendEmails(emailData);
-
-  // const response = await sendgrid.send({
-  //   to: 'carinaavram97@gmail.com',
-  //   from: process.env.MY_EMAIL,
-  //   subject: 'Test Email',
-  //   html: '<h1>This is a test</h1>',
-  // });
-  if (!response.error){
-    return new NextResponse('Failed to send emails', {status: 500});
-  } else {
-    return new NextResponse('Emails sent succesfully', {status: 200})
+        // Uncomment the code block to send emails
+        const response = await sendgrid.send({
+          to: user.email,
+          from: process.env.MY_EMAIL,
+          subject: `Birthday Reminder: ${birthday.name}'s birthday is coming up!`,
+          html: `
+            <html>
+              <head>
+                <style>
+                  body {
+                    font-family: Arial, sans-serif;
+                    background-color: #whitesmoke;
+                    color: #333;
+                  }
+                  h1 {
+                    color: #ff5500;
+                  }
+                  p {
+                    font-size: 16px;
+                    line-height: 1.5;
+                  }
+                </style>
+              </head>
+              <body>
+                <h1>Dear ${user.name},</h1>
+                <p>Just a reminder that ${
+                  birthday.name
+                }'s birthday is coming up on ${formattedDate}.</p>
+                <p>Don't forget to wish ${birthday.name} a happy birthday!</p>
+                <p>Here are some gift ideas: ${birthday.gifts.join(',')}.</p>
+              </body>
+            </html>
+            `,
+        });
+      }
+    }
   }
 }
-
-export {handler as GET};
