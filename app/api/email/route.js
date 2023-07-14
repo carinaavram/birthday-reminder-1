e4529ebffe9c5c0560ee900c1f310f6bd655b11c
carinaavram <carinaavram97@gmail.com>
2023-07-14 11:24:07 -0400

    email

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -7,72 +7,72 @@ const sendgrid = require('@sendgrid/mail');
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
-// Function to retrieve email data
-async function getEmailData() {
-  const emailData = [];
-  dbConnect();
-  const users = await User.find();
-  for (const user of users) {
-    const birthdays = await Birthday.find({ email: user.email });
-    for (const birthday of birthdays) {
-      const birthdayDate = new Date(birthday.date);
-      const formattedDate = birthdayDate.toLocaleDateString('en-US', {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'short',
-        day: 'numeric',
-      });
+// // Function to retrieve email data
+// async function getEmailData() {
+//   const emailData = [];
+//   dbConnect();
+//   const users = await User.find();
+//   for (const user of users) {
+//     const birthdays = await Birthday.find({ email: user.email });
+//     for (const birthday of birthdays) {
+//       const birthdayDate = new Date(birthday.date);
+//       const formattedDate = birthdayDate.toLocaleDateString('en-US', {
+//         weekday: 'long',
+//         year: 'numeric',
+//         month: 'short',
+//         day: 'numeric',
+//       });
 
-      const data = {
-        birthdayName: birthday.name,
-        birthdayDate: formattedDate,
-        gifts: birthday.gifts,
-        userName: user.name,
-        userEmail: user.email,
-      };
-      emailData.push(data);
-    }
-  }
-  return emailData;
-}
+//       const data = {
+//         birthdayName: birthday.name,
+//         birthdayDate: formattedDate,
+//         gifts: birthday.gifts,
+//         userName: user.name,
+//         userEmail: user.email,
+//       };
+//       emailData.push(data);
+//     }
+//   }
+//   return emailData;
+// }
 
-// Function to send the email
-async function sendEmails(emailData) {
-  for (const data of emailData) {
-    const { birthdayName, birthdayDate, gifts, userName, userEmail } = data;
-    await sendgrid.send({
-      to: userEmail,
-      from: process.env.MY_EMAIL,
-      subject: `Birthday Reminder: ${birthdayName}'s birthday is coming up!`,
-      html: `
-        <html>
-          <head>
-            <style>
-              body {
-                font-family: Arial, sans-serif;
-                background-color: #whitesmoke;
-                color: #333;
-              }
-              h1 {
-                color: #ff5500;
-              }
-              p {
-                font-size: 16px;
-                line-height: 1.5;
-              }
-            </style>
-          </head>
-          <body>
-            <h1>Dear ${userName},</h1>
-            <p>Just a reminder that ${birthdayName}'s birthday is coming up on ${birthdayDate}.</p>
-            <p>Don't forget to wish ${birthdayName} a happy birthday!</p>
-            <p>Here are some gift ideas: ${gifts.join(',')}.</p>
-          </body>
-        </html>
-      `,
-    });
-  }
-}
+// // Function to send the email
+// async function sendEmails(emailData) {
+//   for (const data of emailData) {
+//     const { birthdayName, birthdayDate, gifts, userName, userEmail } = data;
+//     await sendgrid.send({
+//       to: userEmail,
+//       from: process.env.MY_EMAIL,
+//       subject: `Birthday Reminder: ${birthdayName}'s birthday is coming up!`,
+//       html: `
+//         <html>
+//           <head>
+//             <style>
+//               body {
+//                 font-family: Arial, sans-serif;
+//                 background-color: #whitesmoke;
+//                 color: #333;
+//               }
+//               h1 {
+//                 color: #ff5500;
+//               }
+//               p {
+//                 font-size: 16px;
+//                 line-height: 1.5;
+//               }
+//             </style>
+//           </head>
+//           <body>
+//             <h1>Dear ${userName},</h1>
+//             <p>Just a reminder that ${birthdayName}'s birthday is coming up on ${birthdayDate}.</p>
+//             <p>Don't forget to wish ${birthdayName} a happy birthday!</p>
+//             <p>Here are some gift ideas: ${gifts.join(',')}.</p>
+//           </body>
+//         </html>
+//       `,
+//     });
+//   }
+// }
 
 export async function GET(req, res) {
   await dbConnect();
@@ -131,6 +131,11 @@ export async function GET(req, res) {
             </html>
             `,
         });
+        if (response.ok) {
+          return new NextResponse('Success' , {status: 200});
+        } else {
+          return new NextResponse('Failed', {status: 500})
+        }
       }
     }
   }
